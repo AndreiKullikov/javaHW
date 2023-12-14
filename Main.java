18e2cc6130c620a43b8b3429fa537fbf4f4bb528
AndreiKullikov <139259228+AndreiKullikov@users.noreply.github.com>
2023-12-14 17:35:31 +0100

    Update Main.java
    test

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         Set<Notebook> set = new HashSet<>();
-        set.add(new Notebook("Notebook 1", 8, "Windows10", 80000, "HP"));
+        set.add(new Notebook("Notebook 1", 8, "Windows11", 89000, "Dell"));
         set.add(new Notebook("Notebook 2", 16, "Windows10", 85000, "Asus"));
-        set.add(new Notebook("Notebook 3", 32, "linux", 85000, "Lenovo"));
+        set.add(new Notebook("Notebook 3", 32, "linux", 85000, "acer"));
         set.add(new Notebook("Notebook 4", 64, "linux", 90000, "Lenovo"));
-
+        set.add(new Notebook("Notebook 5", 32, "mac", 190000, "Apple"));
         OperationWithNotebook operation = new OperationWithNotebook(set);
         operation.start();
 
